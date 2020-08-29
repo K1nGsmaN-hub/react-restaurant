efdb0299c094a217fa4ebfe8b7a2fd6e3370078e
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-30 02:42:47 +0800

    added logic code to MenuList and connect with redux

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,66 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+
 import MenuListItem from '../menu-list-item';
+import WithRestoService from "../hoc";
+import {menuLoaded, menuRequested, menuError} from "../../actions";
+import Spinner from "../spinner";
+import Error from "../error";
 
 import './menu-list.scss';
 
 class MenuList extends Component {
+    componentDidMount() {
+        this.props.menuRequested()
+
+        const {RestoService} = this.props
+        const menus = new RestoService()
+
+        menus.getMenuItems()
+            .then(res => this.props.menuLoaded(res))
+            .catch(error => this.props.menuError())
+    }
 
     render() {
+        const {menuItems, loading, error} = this.props
+
+        if (error) {
+            return <Error />
+        }
+
+        if (loading) {
+            return <Spinner/>
+        }
+
+        const menuElems = menuItems.map(item => <MenuListItem key={item.id} menuItem={item}/>)
 
         return (
-            <ul className="menu__list">
-                <MenuListItem/>
-            </ul>
+            <View elems={menuElems}/>
         )
     }
-};
+}
+
+const View = ({elems}) => {
+    return (
+        <ul className="menu__list">
+            {elems}
+        </ul>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        menuItems: state.menu,
+        loading: state.loading,
+        error: state.error
+    }
+}
+
+const mapDispatchToProps = {
+    menuLoaded,
+    menuRequested,
+    menuError
+}
 
 
-export default MenuList;
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList))
